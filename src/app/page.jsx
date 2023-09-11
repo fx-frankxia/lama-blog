9813c452d7c5d32e443bce7e6382b97a84a939e5
FRANK XIA <FX.FRANKXIA@GMAIL.COM>
2023-09-11 11:21:21 +0800

    global css set up

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,22 @@
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod quae hic
+        veritatis amet aspernatur molestiae magnam facilis alias corrupti odit?
+        Quae, ducimus! Facere obcaecati explicabo omnis expedita distinctio ab?
+        Dolores?
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ullam impedit at deserunt, error reiciendis dicta perferendis, quos dolore quasi incidunt. Esse eaque enim nostrum cumque veritatis molestiae dolores id!
+        Inventore non totam eaque. Nemo quidem eum porro exercitationem ipsam eveniet, eligendi qui sed, aliquid ea perferendis numquam fuga ut commodi id. Est ex ut, atque sapiente qui beatae consectetur?
+        Quia expedita dolore eos a veniam esse consequuntur consequatur consectetur officiis hic temporibus optio, ducimus saepe commodi nobis, totam, quam eius recusandae architecto excepturi possimus. Velit officia repellendus autem nam?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel dolores, possimus, perspiciatis nulla repellendus a explicabo saepe ea repudiandae incidunt eligendi odio voluptatibus distinctio quos id expedita numquam suscipit consequuntur!
+        Dolor, sint consectetur. Molestias optio voluptatem soluta, tempore rerum adipisci provident praesentium voluptatum blanditiis ipsum velit, iste quidem eaque minus officiis quos quam consequatur aperiam mollitia libero! Non, amet unde!
+        Cupiditate laudantium iste eum, inventore quibusdam quis debitis. Totam, quaerat? Provident accusamus, adipisci dolores nobis aliquid repudiandae blanditiis dolore quas impedit debitis perspiciatis inventore non, facere dignissimos mollitia obcaecati veniam!
+      </p>
+    </div>
+  );
 }
